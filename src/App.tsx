be6902370e50606777ff1a06dd9e15c316c256f3
Guilherme Barros <g.henrique.barros21@gmail.com>
2024-02-20 17:50:46 -0300

    create past components and add the route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,46 @@
 
+import { createBrowserRouter } from "react-router-dom"
+import Dashboard from "./pages/dashboard"
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Register from "./pages/register"
+import NovoCarro from "./pages/dashboard/novo"
+import Detalhes from "./pages/carros"
+
+import { Layout } from "./components/layout"
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/carro/:id',
+        element: <Detalhes />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      },
+      {
+        path: '/dashboard/novo',
+        element: <NovoCarro/>
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  }
+])
+
 
 function App() {
 
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../header";
+import { Outlet } from "react-router-dom"
+
+export const Layout = () => {
+    return ( 
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+     );
+}
+ 
