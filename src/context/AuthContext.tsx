d98ebb7cde36dc9feec0e851fc028c8b85a3ff11
Guilherme Barros <g.henrique.barros21@gmail.com>
2024-02-22 14:47:41 -0300

    realmente terminando o authcontext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,10 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import { ReactNode, createContext, useState, useEffect } from 'react'
+import { auth } from '../services/firebase';
 
 type AuthContextData = {
     signed: boolean;
+    loadingAuth: boolean;
 }
 
 interface AuthProviderProps {
@@ -10,8 +13,8 @@ interface AuthProviderProps {
 
 interface UserProps {
     uid: string;
-    name: string;
-    email: string;
+    name: string | null;
+    email: string | null;
 }
 
 export const AuthContext = createContext({} as AuthContextData)
@@ -20,9 +23,32 @@ export const AuthContext = createContext({} as AuthContextData)
 const AuthProvider = ({ children }: AuthProviderProps) => {
 
     const [user, setUser] = useState<UserProps | null>(null);
+    const [loadingAuth, setLoadingAuth] = useState(true)
+    useEffect(() => {
+        const olheiro = onAuthStateChanged(auth, (user) => {
+            if(user) {
+                setUser({
+                    uid: user.uid,
+                    name: user?.displayName,
+                    email: user?.email
+                })
+
+                setLoadingAuth(false)
+
+            }else{
+                setUser(null);
+                setLoadingAuth(false)
+            }
+        })
+
+        return () => {
+            olheiro();
+        }
+
+    }, [])
     
     return (
-        <AuthContext.Provider value={{ signed: !!user }}>
+        <AuthContext.Provider value={{ signed: !!user, loadingAuth, }}>
             {children}
         </AuthContext.Provider>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { router } from './App.tsx'
 import './index.css'
-
 import { RouterProvider } from "react-router-dom"
+import AuthProvider from './context/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
 )
